b718089adca761679cae85ab1c4910e2afc79eff
Ruchitha sai Rasoju <ruchithasai.r188@gmail.com>
2026-03-16 09:44:01 +0530

    Set up basic Node.js auth server with Express

--- node-server/index.js
+++ node-server/index.js
@@ -1,1 +1,16 @@
+const express = require("express");
+const authRoutes = require("./routes/auth");
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.get("/api/auth/test", (req, res) => {
+  res.json({ message: "Node auth server is working!" });
+});
+
+app.listen(3001, () => {
+  console.log("Node auth server running on port 3001");
+});
 
