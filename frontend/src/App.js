cf0df4becca1f151eb5b6a0c73abac70a0b482e0
Ruchitha sai Rasoju <ruchithasai.r188@gmail.com>
2026-03-16 09:46:24 +0530

    Set up routing for application pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
+import SkillGapPage from "./pages/SkillGapPage";
+import RoadmapPage from "./pages/RoadmapPage";
+import QuizPage from "./pages/QuizPage";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/skillgap" element={<SkillGapPage />} />
+          <Route path="/roadmap" element={<RoadmapPage />} />
+          <Route path="/quiz" element={<QuizPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
 
