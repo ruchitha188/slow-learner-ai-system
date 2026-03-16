0f19fc0ae232f76622662e5cfc8d3e88ff8352be
Ruchitha sai Rasoju <ruchithasai.r188@gmail.com>
2026-03-16 10:16:33 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
-from backend.routes.student_routes import student_bp
-from backend.routes.analysis_routes import analysis_bp
-from backend.routes.roadmap_routes import roadmap_bp
-from backend.routes.quiz_routes import quiz_bp
+from routes.student_routes import student_bp
+from routes.analysis_routes import analysis_bp
+from routes.roadmap_routes import roadmap_bp
+from routes.quiz_routes import quiz_bp
 
 app = Flask(__name__)
 CORS(app)
 
-# Register blueprints
 app.register_blueprint(student_bp, url_prefix="/api/students")
 app.register_blueprint(analysis_bp, url_prefix="/api/analysis")
 app.register_blueprint(roadmap_bp, url_prefix="/api/roadmap")
@@ -16,11 +15,10 @@ app.register_blueprint(quiz_bp, url_prefix="/api/quiz")
 
 @app.route("/api/test")
 def test():
-    return {
+    return jsonify({
         "message": "Backend is working!",
         "project": "AI System to Identify and Support Slow Learners"
-    }
+    })
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
-
